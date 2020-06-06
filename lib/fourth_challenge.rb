7fdaa2a71c1a4dc6d7e023110518b621bb7ca7cf
edmundo papi <edmundopierre@yahoo.com>
2020-06-06 11:42:40 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -3,7 +3,7 @@ def fourth_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "dead"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
         {name: "Benvolio", age: "17", attitude: "worried"},
@@ -13,7 +13,7 @@ def fourth_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "dead"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
         {name: "Steven", age: "30", attitude: "confused"},
