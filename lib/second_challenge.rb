8df1341e68591de6a41ae7a5acc419d587abad69
edmundo papi <edmundopierre@yahoo.com>
2020-06-06 11:13:30 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,9 +2,15 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {  
+      :name => "Lord Montague",
+      :age => "53"
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
       }}
 
   
